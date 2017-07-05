0b555207fd3e3e88796d6637c8450b57bbb56365
Matthew Daumas <matthewdaumas@Matthews-MacBook-Pro.local>
2017-07-05 16:19:00 -0500

    facebook log in Please enter the commit message for your changes. Lines starting

---
+++ ExploreApp/Constants.swift
@@ -0,0 +1,11 @@
+//
+//  Constants.swift
+//  MapGame
+//
+//  Created by Matthew Daumas on 6/18/17.
+//  Copyright © 2017 Matthew Daumas. All rights reserved.
+//
+
+import UIKit
+
+let SHADOW_GRAY: CGFloat = 120.0 / 255.0
